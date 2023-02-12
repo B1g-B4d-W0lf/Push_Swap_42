1e8b9750848da17f6c98f763c8d56c6c6dd695cf
Big Bad Wolf <118118837+B1g-B4d-W0lf@users.noreply.github.com>
2023-02-12 04:35:15 +0100

    parsiparsiparsi

--- push_swap.c
+++ push_swap.c
@@ -1,5 +1,11 @@
 #include "push_swap.h"
 
+void	printerror(void)
+{
+	ft_printf("ERROR BITCH TRY AGAIN");
+	exit(0);
+}
+
 void	clearlst(t_list **lst)
 {
 	t_list	*temp;
@@ -10,8 +16,8 @@ void	clearlst(t_list **lst)
 	{	
 		temp = *lst;
 		*lst = (*lst)->next;
-		free(temp->content);
-		free(lst);
+		temp->content = 0;
+		free(temp);
 	}
 	return ;
 }
@@ -41,69 +47,63 @@ int	checkargv(int argc, char **argv)
 	return(1);
 }
 
-t_list	**string(char *arg)
+t_list	*string(char *arg)
 {
-	t_list	**tab;
+	t_list	*tab;
 	char	**temp;
-	int		*str;
+	int		str;
 	int		i;
 
+	tab = NULL;
 	temp = ft_split(arg, ' ');
 	i = 0;
-	str = malloc(sizeof(int));
-	if(!str)
-		free(str);
+	while(temp[i] != NULL)
+		i++;
+	if (!checkargv(i, temp))
+		printerror();
+	i = 0;
 	while(temp[i] != NULL)
 	{
-		*str = ft_atoi(temp[i]);
-		printf("%d\n", *str);
-		ft_lstadd_back(tab, ft_lstnew(str));
-		if(!tab || !*tab)
-			free(tab);
+		str = ft_atoi(temp[i]);
+		printf("%d\n", str);
+		ft_lstadd_back(&tab, ft_lstnew(str));
+		printf("%d--\n", tab->content);
 		i++;
 	}
-	free(str);
 	return (tab);
 }
 
-t_list	**array(int argc, char **argv)
+t_list	*array(int argc, char **argv)
 {
-	t_list	**tab;
+	t_list	*tab;
 	int		i;
-	int		*j;
+	int		j;
 
+	tab = NULL;
 	i = 1;
-	j = malloc(sizeof(int));
-	if (!j)
-		free(j);
 	while(i <= argc)
 	{
-		*j = ft_atoi(argv[i]);
-		printf("%d\n", *j);
-		ft_lstadd_back(tab, ft_lstnew(j));
-		if(!tab || !*tab)
-			free(tab);
-		printf("%d\n", (*tab)->content);
+		j = ft_atoi(argv[i]);
+		printf("%d\n", j);
+		ft_lstadd_back(&tab, ft_lstnew(j));
+		printf("%d--\n", tab->content);
 		i++;
 	}
-	free(j);
 	return (tab);
 }
 
 int	main(int argc, char **argv)
 {
-	t_list	**lst;
+	t_list	*lst;
 
 	if (checkargv(argc, argv) == 0)
-	{
-		ft_printf("ERROR BITCH\n");
-		return(0);
-	}
-	else if(argc > 2)
-		lst = array(argc, argv);
+		printerror();
 	else if(argc == 2)
 		lst = string(argv[1]);
-	clearlst(lst);
-	ft_printf("%d\n", (*lst)->content);
+	else if(argc > 2)
+		lst = array(argc, argv);	
+	printf("c bon ou c pa bon\n");
+	printf("%d\n", lst->content);
+	clearlst(&lst);
 	return (0);
 }
